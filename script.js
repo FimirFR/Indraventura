87878373caf8f5faa6d1330d409e9b3cea7a65f2
FimirFR <projet.perle@free.fr>
2026-04-24 19:27:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+const canvas = document.getElementById("confetti");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let confetti = [];
+
+function createConfetti() {
+    for (let i = 0; i < 120; i++) {
+        confetti.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height - canvas.height,
+            r: Math.random() * 6 + 2,
+            d: Math.random() * 10 + 5,
+            color: `hsl(${Math.random() * 360}, 70%, 60%)`
+        });
+    }
+}
+
+function drawConfetti() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    confetti.forEach(c => {
+        ctx.beginPath();
+        ctx.fillStyle = c.color;
+        ctx.fillRect(c.x, c.y, c.r, c.r);
+
+        c.y += c.d;
+
+        if (c.y > canvas.height) {
+            c.y = -10;
+            c.x = Math.random() * canvas.width;
+        }
+    });
+
+    requestAnimationFrame(drawConfetti);
+}
+
 const map = L.map('map').setView([46.597509, 1.599967], 18);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
